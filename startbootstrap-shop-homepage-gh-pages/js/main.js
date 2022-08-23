5beb3838f9bfdf19760a84f2ace91504a0246ae3
IgnacioVarisco <varisconacho@gmail.com>
2022-08-23 12:37:20 -0300

    agregar al carrito con evento

--- startbootstrap-shop-homepage-gh-pages/js/main.js
+++ startbootstrap-shop-homepage-gh-pages/js/main.js
@@ -49,7 +49,7 @@
 // borrarProductoDelCarrito(2);
 // borrarProductoDelCarrito(3);
 
-
+const carrito = []
 
 const productos = [
     {id: 1,  
@@ -60,14 +60,14 @@ const productos = [
 
     {id: 2,  
         name:"zapa adadis" ,
-        img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a42a5d53-2f99-4e78-a081-9d07a2d0774a/air-force-1-07-zapatillas-xCxt0q.png",
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/8da8ca5de56c433ba559ad7800e4a4d1_9366/Zapatillas_Courtflash_para_Tenis_Negro_GW2988_01_standard.jpg",
         price: 900 ,}, 
 
 
 
    {id: 3,  
     name:"zapa topito",
-    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a42a5d53-2f99-4e78-a081-9d07a2d0774a/air-force-1-07-zapatillas-xCxt0q.png",
+    img: "https://ferreira.vteximg.com.br/arquivos/ids/226901-588-588/to_83301.jpg?v=636615700832270000",
     price: 900 ,},
 ];
 
@@ -86,7 +86,7 @@ productos.forEach((producto) => {
     <div class="card-body">
       <h5 class="card-title"> ${producto.name}</h5>
       <p class="card-text"> ${producto.price}</p>
-      <button id =${idButton}> agregar al carrito </button>
+      <button id =${idButton}  data-id =" ${producto.id}" > agregar al carrito </button>
     </div>
   </div>` ;
 
@@ -99,8 +99,18 @@ productos.forEach((producto) => {
 
 productos.forEach((producto) => {
     const idButton = `add-cart${producto.id}`
-    document.getElementById(idButton).addEventListener("click" , () => {
-        alert ("holaa");
+    document.getElementById(idButton).addEventListener("click" , (event) => {
+
+//        const nodo = event.target ;
+//        const idProducto = nodo.getAttribute("data-id");
+//       const indiceProducto= productos.findIndex(producto =>producto.id == idProducto)
+//    const producto = productos [indiceProducto];
+
+   carrito.push(producto);
+
+   console.log ( carrito)
+       // console.log(nodo.getattribute("data-id"))
+        
       })
 });
 
