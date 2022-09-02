78742ea006e4188324689808a2617d0e37c6c908
IgnacioVarisco <varisconacho@gmail.com>
2022-09-02 11:32:10 -0300

    carrito pop up terminado falta aside

--- startbootstrap-shop-homepage-gh-pages/js/main.js
+++ startbootstrap-shop-homepage-gh-pages/js/main.js
@@ -7,6 +7,9 @@
      
       informacion.results.forEach((producto) => {
         console.log(producto)
+
+
+        
        
       const idButton = `${producto.id}`;
       console.log(idButton);
@@ -17,14 +20,15 @@
         <p class="card-text"> -$${producto.price}</p>
         <button id =${idButton}  data-id =" ${producto.id}"   > agregar al carrito </button>
       </div>
-    </div>`
+    </div>`;
+
+
+
+
+    
       })
-   
-  
 
 
-
- 
 
 
 
@@ -34,62 +38,75 @@ const total = carrito.reduce((acumulador , producto) => acumulador + producto.pr
 document.getElementById("total-cart").innerHTML = carrito.length + "$ " + total;
 
 
-function generarCardsCarrito (){
-  carrito.forEach((producto) => {
-    document.getElementById("modal-carrito").innerHTML += `
-    <div class="container">
-  <div class="row">
-    <div class="col">
-      nombre 
-    </div>
-    <div class="col">
-      precio
-    </div>
-    <div class="col">
-    imagen
-  </div>
-  </div>
-  <div class="row">
-    <div class="col">
-     ${producto.title}
-    </div>
-    <div class="col">
-    ${producto.price}
-    </div>
-    <div class="col">
-    <img src= "${producto.thumbnail}" class="card-img-top" alt="...">
-    </div>
+//CARRITO POP UP
 
-  </div>
-  </div>
-  `;
-  })
-}
+carrito.forEach((producto)=> {
+  document.getElementById("carritoPopUP").innerHTML += `
+<div class="container">
+<div class="row">
+<div class="col">
+  nombre 
+</div>
+<div class="col">
+  precio
+</div>
+<div class="col">
+imagen
+</div>
+</div>
+<div class="row">
+<div class="col">
+ ${producto.title}
+</div>
+<div class="col">
+${producto.price}
+</div>
+<div class="col">
+<img src= "${producto.thumbnail}" class="card-img-top" alt="...">
+</div>
 
-
- 
+</div>
+</div>
+`; })
 
 
 
 
 
+// document.getElementById("carritoLista").innerHTML = ""
+// carrito.forEach((producto) => {
+//  document.getElementById("carritoLista").innerHTML += `
+//  <div class="container">
+// <div class="row">
+//  <div class="col">
+//    nombre 
+//  </div>
+//  <div class="col">
+//    precio
+//  </div>
+//  <div class="col">
+//  imagen
+// </div>
+// </div>
+// <div class="row">
+//  <div class="col">
+//   ${producto.title}
+//  </div>
+//  <div class="col">
+//  ${producto.price}
+//  </div>
+//  <div class="col">
+//  <img src= "${producto.thumbnail}" class="card-img-top" alt="...">
+//  </div>
 
-//crear cards
+// </div>
+// </div>
+// `;
 
-// productos.forEach((producto) => {
-//     const idButton = `add-cart${producto.id}`;
-//     console.log(idButton);
-//     document.getElementById ("cards-index").innerHTML += ` <div class="card x-4 px-lg-5 my-5 col-4  text-center container-fluid">
-//     <img src= "${producto.img}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//       <h5 class="card-title"> ${producto.name}</h5>
-//       <p class="card-text"> ${producto.price}</p>
-//       <button id =${idButton}  data-id =" ${producto.id}" > agregar al carrito </button>
-//     </div>
-//   </div>` ;
 
-// })
 
+
+// })  
 
 
 
@@ -104,72 +121,61 @@ informacion.results.forEach((producto) => {
    localStorage.setItem("carrito", JSON.stringify(carrito));
    const total = carrito.reduce((acumulador , producto) => acumulador + producto.price, 0 );
    document.getElementById("total-cart").innerHTML = `${carrito.length}  -$${total}`;
-   document.getElementById("carritoLista").innerHTML = ""
-   carrito.forEach((producto) => {
-    document.getElementById("carritoLista").innerHTML += `
-    <div class="container">
-  <div class="row">
-    <div class="col">
-      nombre 
-    </div>
-    <div class="col">
-      precio
-    </div>
-    <div class="col">
-    imagen
-  </div>
-  </div>
-  <div class="row">
-    <div class="col">
-     ${producto.title}
-    </div>
-    <div class="col">
-    ${producto.price}
-    </div>
-    <div class="col">
-    <img src= "${producto.thumbnail}" class="card-img-top" alt="...">
-    </div>
-
-  </div>
-  </div>
-  `;
 
 
 
-  document.getElementById("pollito").innerHTML += `
-  <div class="container">
-<div class="row">
-  <div class="col">
-    nombre 
-  </div>
-  <div class="col">
-    precio
-  </div>
-  <div class="col">
-  imagen
-</div>
-</div>
-<div class="row">
-  <div class="col">
-   ${producto.title}
-  </div>
-  <div class="col">
-  ${producto.price}
-  </div>
-  <div class="col">
-  <img src= "${producto.thumbnail}" class="card-img-top" alt="...">
-  </div>
 
-</div>
-</div>
-`;
-  })    
-  })   
-  })
 
+
+
+
+   JSON.parse(localStorage.getItem("carrito"));
+   document.getElementById("carritoPopUP").innerHTML = ""
+   carrito.forEach((producto)=> {document.getElementById("carritoPopUP").innerHTML += `
+   <div class="container">
+   <div class="row">
+   <div class="col">
+     nombre 
+   </div>
+   <div class="col">
+     precio
+   </div>
+   <div class="col">
+   imagen
+   </div>
+   </div>
+   <div class="row">
+   <div class="col">
+    ${producto.title}
+   </div>
+   <div class="col">
+   ${producto.price}
+   </div>
+   <div class="col">
+   <img src= "${producto.thumbnail}" class="card-img-top" alt="...">
+   </div>
+   
+   </div>
+   </div>
+   `; })
    
 
 
+
+    //MOSTRAR CARRITO EN BODY
+    
+  })   
+ 
+
+ }
+  
+  
+  
+  
+  
+  )
+
+   
 
 
 
